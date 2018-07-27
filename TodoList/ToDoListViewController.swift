164f56a6e7c2bcbf9a218edfba97a6829a42b315
Nicholas Richardson <nicholasrichardson@nicholass-macbook-pro.local>
2018-07-26 20:21:42 -0400

    To Do list complete with datasource and delegate methods

---
+++ TodoList/ToDoListViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Nicholas Richardson on 7/26/18.
+//  Copyright © 2018 Nicholas Richardson. All rights reserved.
+//
+
+import UIKit
+
+class ToToListViewController: UITableViewController {
+
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demagorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    // MARK - Tableview Datasource methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+   // MARK - Tableview Delegate Method
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true) // makes it flash gray when clicked then return to white
+        
+    }
+
+
+}
+
